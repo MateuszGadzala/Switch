78eda5c75f85c9adbafb7ca8952a36edaa676daf
Mateusz <99211845+MateuszGadzala@users.noreply.github.com>
2024-08-22 22:56:43 +0200

    Add files via upload

---
+++ Switch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Switch
+{
+    class SwitchClass
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Its Monday!");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("The last day of the " +
+                        "week");
+                    break;
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Its weekend!");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Its weekend!");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the " +
+                        "work week!");
+                    break;
+            }
+        }
+    }
+}
+namespace CodingExcersise
+{
+    public class ParkingCalculator
+    {
+        public static double Xxx(int hours)
+        {
+            int result = 0;
+            int fee = 5;    
+
+            switch(hours)
+            {
+                case 1:
+                    result = fee;
+                    break;
+                default:
+                    result = fee + (hours - 1) * 3;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
